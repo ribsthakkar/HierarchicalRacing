e7152d27f56cb10624b8a1a4367fd8a39c0254ac
ribsthakkar <rishabh.thakkar@gmail.com>
2021-04-11 23:05:33 -0500

    Simple Model Working with Storm CLI

--- highlevel_strategy_mdp.py
+++ highlevel_strategy_mdp.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from typing import Dict, List, TextIO
 
 from util import _write_with_newline_and_sc, neg_to_str
-import stormpy
 MAX_TIRE_AGE = 100
+RUN_STORMPY = False
 class CarDef:
     def __init__(self, max_velocity, velocity_step, min_gs, max_gs, max_braking, max_acceleration, init_tire, init_time, init_line, init_velocity, init_position):
         self.max_v = max_velocity
@@ -24,10 +24,10 @@ class TrackComponent:
     def __init__(self, length):
         self.length = length
 
-    def is_v_feasible(self, velocity, tire_wear, min_cornering_gs, max_cornering_gs):
+    def is_v_feasible(self, velocity, line, tire_wear, min_cornering_gs, max_cornering_gs):
         raise NotImplementedError("Must be implemented by child classes")
 
-    def tire_wear(self, velocity):
+    def tire_wear(self, velocity, line):
         raise NotImplementedError("Must be implemented by child classes")
 
 
@@ -35,47 +35,47 @@ class TrackStraight(TrackComponent):
     def __init__(self, length):
         super().__init__(length)
 
-    def is_v_feasible(self, velocity, tire_wear, min_cornering_gs, max_cornering_gs):
+    def is_v_feasible(self, velocity, line, tire_wear, min_cornering_gs, max_cornering_gs):
         return True
 
-    def tire_wear(self, velocity):
+    def tire_wear(self, velocity, line):
         if velocity == 0: return 0
         return 1
 
 class TrackCorner:
     class Entry(TrackComponent):
-        def __init__(self, length, turn_radius):
+        def __init__(self, length, turn_radius, width, num_lines):
             super().__init__(length)
-            self.tr = turn_radius
+            self.tr = list(map(lambda l: turn_radius + l*width/num_lines, range(num_lines)))
 
-        def is_v_feasible(self, velocity, tire_wear, min_cornering_gs, max_cornering_gs):
-            gs = (velocity**2)/self.tr
+        def is_v_feasible(self, velocity, line, tire_wear, min_cornering_gs, max_cornering_gs):
+            gs = (velocity**2)/self.tr[line]
             g_diff = (max_cornering_gs-min_cornering_gs)*(tire_wear/MAX_TIRE_AGE)
             return gs <= max_cornering_gs-g_diff
 
-        def tire_wear(self, velocity):
-            gs = (velocity**2)/self.tr
+        def tire_wear(self, velocity, line):
+            gs = (velocity**2)/self.tr[line]
             return int((math.ceil(gs) * self.length/velocity)/10)
 
 
     class Mid(TrackComponent):
-        def __init__(self, length, turn_radius):
+        def __init__(self, length, turn_radius, width, num_lines):
             super().__init__(length)
-            self.tr = turn_radius
+            self.tr = list(map(lambda l: turn_radius + l*width/num_lines, range(num_lines)))
 
-        def is_v_feasible(self, velocity, tire_wear, min_cornering_gs, max_cornering_gs):
-            gs = (velocity**2)/self.tr
+        def is_v_feasible(self, velocity, line, tire_wear, min_cornering_gs, max_cornering_gs):
+            gs = (velocity**2)/self.tr[line]
             g_diff = (max_cornering_gs-min_cornering_gs)*(tire_wear/MAX_TIRE_AGE)
             return gs <= max_cornering_gs-g_diff
 
-        def tire_wear(self, velocity):
-            gs = (velocity**2)/self.tr
+        def tire_wear(self, velocity, line):
+            gs = (velocity**2)/self.tr[line]
             return int((math.ceil(gs) * self.length/velocity)/100)
 
 
-    def __init__(self, turn_radius, entry_length, mid_length, exit_length):
-        self.entry = self.Entry(entry_length, turn_radius)
-        self.mid = self.Mid(mid_length, turn_radius)
+    def __init__(self, num_lines, width, inside_turn_radius, entry_length, mid_length, exit_length):
+        self.entry = self.Entry(entry_length, inside_turn_radius, width, num_lines)
+        self.mid = self.Mid(mid_length, inside_turn_radius, width, num_lines)
         self.exit = TrackStraight(exit_length)
 
 
@@ -115,19 +115,19 @@ class TrackDef:
                     guard = f"track_line={tl} & {ta}<=tire_age & tire_age<{ta+tire_wear_step} & {v}<=velocity & velocity<{v+velocity_step}"
                     avg_v = v+velocity_step/2
                     avg_ta = ta+tire_wear_step/2
-                    avg_dist = math.sqrt((straight.length/2)**2 + (abs(tl - line)*(self.width)/(self.track_lines+1))**2)
+                    avg_dist = math.sqrt((straight.length)**2 + (abs(tl - line)*(self.width)/(self.track_lines+1))**2)
                     avg_final_v = (math.sqrt(max(0, avg_v**2 + 2*acc*avg_dist)))
                     max_fv = min(car_def.max_v, int(avg_final_v + 4))
                     min_fv = max(1, int(avg_final_v - 4))
                     feasible_v = []
                     for fv in range(min_fv, max_fv+1):
-                        if entry.is_v_feasible(fv, avg_ta, car_def.min_gs, car_def.max_gs):
+                        if entry.is_v_feasible(fv, tl, avg_ta, car_def.min_gs, car_def.max_gs):
                             feasible_v.append(fv)
                     for fv in feasible_v:
                         est_dt = int((2 * avg_dist / (avg_v + fv)))
-                        est_dta = entry.tire_wear(fv)
+                        est_dta = entry.tire_wear(fv, tl)
                         prob = f'1/{len(feasible_v)}'
-                        changes = f"{tp_update_string} & (velocity'={fv}) & (track_line'={line}) & (tire_age'=tire_age+{est_dta}) & (t'=t+{est_dt}) {lap_change_update}"
+                        changes = f"{tp_update_string} & (velocity'={fv}) & (track_line'={line}) & (tire_age'=tire_age+{est_dta}) & (t'=t+{est_dt}) & (dt'={est_dt}) {lap_change_update}"
                         updates.append((prob, changes))
 
                     if len(updates):
@@ -138,27 +138,27 @@ class TrackDef:
                     avg_v = car_def.max_v
                     avg_ta = ta + tire_wear_step / 2
                     avg_dist = math.sqrt(
-                        (straight.length / 2) ** 2 + (abs(tl - line) * (self.width) / (self.track_lines + 1)) ** 2)
+                        (straight.length) ** 2 + (abs(tl - line) * (self.width) / (self.track_lines + 1)) ** 2)
                     avg_final_v = (math.sqrt(max(0, avg_v**2 + 2*acc*avg_dist)))
                     max_fv = min(car_def.max_v, int(avg_final_v + 4))
                     min_fv = max(1, int(avg_final_v - 4))
                     feasible_v = []
                     for fv in range(min_fv, max_fv + 1):
-                        if entry.is_v_feasible(fv, avg_ta, car_def.min_gs, car_def.max_gs):
+                        if entry.is_v_feasible(fv, tl, avg_ta, car_def.min_gs, car_def.max_gs):
                             feasible_v.append(fv)
                     for fv in feasible_v:
                         est_dt = int((2 * avg_dist / (avg_v + fv)))
-                        est_dta = entry.tire_wear(fv)
+                        est_dta = entry.tire_wear(fv, tl)
                         prob = f'1/{len(feasible_v)}'
-                        changes = f"{tp_update_string} & (velocity'={fv}) & (track_line'={line}) & (tire_age'=tire_age+{est_dta}) & (t'=t+{est_dt}) {lap_change_update}"
+                        changes = f"{tp_update_string} & (velocity'={fv}) & (track_line'={line}) & (tire_age'=tire_age+{est_dta}) & (t'=t+{est_dt}) & (dt'={est_dt}) {lap_change_update}"
                         updates.append((prob, changes))
                     if len(updates):
                         output.append((guard, updates))
             updates = []
-            guard = f"tire_age >=1000"
+            guard = f"tire_age >={MAX_TIRE_AGE}"
             est_dt = int(straight.length / 1)
             prob = '1'
-            changes = f"{tp_update_string} & (velocity'={1}) & (track_line'={line}) & (tire_age'=tire_age) & (t'=t+{est_dt}) {lap_change_update}"
+            changes = f"{tp_update_string} & (velocity'={1}) & (track_line'={line}) & (tire_age'=tire_age) & (t'=t+{est_dt}) & (dt'={est_dt}) {lap_change_update}"
             updates.append((prob, changes))
             output.append((guard, updates))
         return output
@@ -184,7 +184,7 @@ class TrackDef:
         #     print("Invalid Connection")
         #     exit(1)
 
-def generate_racecar_module(output_file, id, max_time, laps, track_definition, car_definition):
+def generate_racecar_module(output_file, max_time, laps, track_definition, car_definition):
     with open(output_file, "w+") as output:
         _write_with_newline_and_sc("mdp", output, False)
 
@@ -196,12 +196,13 @@ def generate_racecar_module(output_file, id, max_time, laps, track_definition, c
         init_line = car_definition.init_line
         init_v = car_definition.init_velocity
         init_pos = car_definition.init_position
-        _write_with_newline_and_sc(f'module racecar{id}\n', output, False)
+        _write_with_newline_and_sc(f'module racecar{1}\n', output, False)
         _write_with_newline_and_sc(f't : [0..{max_time}] init {init_time}', output)
+        _write_with_newline_and_sc(f'dt : [0..{max_time}] init 0', output)
         _write_with_newline_and_sc(f'lap : [0..{laps}] init 0', output)
         _write_with_newline_and_sc(f'track_pos : [0..{tps-1}] init {init_pos}', output)
         _write_with_newline_and_sc(f'track_line : [0..{tls-1}] init {init_line}', output)
-        _write_with_newline_and_sc(f'tire_age : [0..{MAX_TIRE_AGE}] init {init_tire}', output)
+        _write_with_newline_and_sc(f'tire_age : [0..{MAX_TIRE_AGE+100}] init {init_tire}', output)
         _write_with_newline_and_sc(f'velocity : [1..{max_v}] init {init_v}', output)
         _write_with_newline_and_sc("", output, False)
 
@@ -216,53 +217,55 @@ def generate_racecar_module(output_file, id, max_time, laps, track_definition, c
                         guard = f"track_pos={track_pos} & {ta_v_guard} & lap<{laps}"
                         _write_with_newline_and_sc(f"{action} {guard} -> {'+'.join(map(lambda pair: f'{pair[0]}:{pair[1]}', updates))}", output)
 
-        tp_update_string = "(track_pos'=0)"
         action = f"[pit]"
         guard = f"track_pos={tps-1} & lap<{laps}"
         _write_with_newline_and_sc(
-            f"{action} {guard} -> 1:(track_pos'={track_def.pit_exit_p}) + (tire_age'=0) & (velocity'={min(track_def.pit_exit_v, max_v)}) & (track_line'={track_def.pit_exit_l}) & (t'=t+{track_def.pit_time}) & (lap'=lap+1)", output)
+            f"{action} {guard} -> 1:(track_pos'={track_def.pit_exit_p}) & (tire_age'=0) & (velocity'={min(track_def.pit_exit_v, max_v)}) & (track_line'={track_def.pit_exit_l}) & (t'=t+{track_def.pit_time}) & (lap'=lap+1)", output)
 
         _write_with_newline_and_sc("endmodule", output, False)
         _write_with_newline_and_sc("", output, False)
         _write_with_newline_and_sc(f'label \"goal\" = lap={laps}', output)
 
         _write_with_newline_and_sc("rewards \"total_time\"", output, False)
-        _write_with_newline_and_sc(f"lap<{laps}: t", output)
-        _write_with_newline_and_sc(f"lap={laps}: t", output)
+        _write_with_newline_and_sc(f"lap<{laps}: dt", output)
         _write_with_newline_and_sc("endrewards", output, False)
 
 if __name__ == "__main__":
-    components = [TrackStraight(50), TrackCorner(25, 39, 39, 23), TrackCorner(25, 39, 39, 50), TrackStraight(50), TrackCorner(25, 39, 39,23), TrackCorner(25, 39, 39, 50)]
-    # components = [TrackStraight(500), TrackCorner(250, 390, 390, 226)]
+    NUM_LINES = 2
+    WIDTH = 5
+    components = [TrackStraight(50), TrackCorner(NUM_LINES, WIDTH, 25, 39, 39, 23), TrackCorner(NUM_LINES, WIDTH, 25, 39, 39, 50), TrackStraight(50), TrackCorner(NUM_LINES, WIDTH, 25, 39, 39,23), TrackCorner(NUM_LINES, WIDTH, 25, 39, 39, 50)]
+    # components = [TrackStraight(50), TrackCorner(NUM_LINES, WIDTH, 25, 39, 39, 23)]
     pit_exit_p = 1
     pit_exit_v = 3
     pit_exit_line = 0
     pit_time = 20
-    track_def = TrackDef(components, width=5, pit_exit_position=pit_exit_p, pit_exit_velocity=pit_exit_v, pit_exit_line=pit_exit_line, pit_time=pit_time)
+    track_def = TrackDef(components, width=WIDTH, pit_exit_position=pit_exit_p, pit_exit_velocity=pit_exit_v, pit_exit_line=pit_exit_line, pit_time=pit_time, num_lines=NUM_LINES)
     car_def = CarDef(max_velocity=10, velocity_step=1, min_gs=0.3*9.8, max_gs=1*9.8, max_braking=5, max_acceleration=5,
-                     init_time=0, init_tire=0, init_line=1, init_velocity=2, init_position=0)
+                     init_time=0, init_tire=0, init_line=0, init_velocity=3, init_position=0)
     print(datetime.now())
     print("Generating Prism Program...")
-    generate_racecar_module('result2.txt', id=1, max_time=100, laps=1, track_definition=track_def, car_definition=car_def)
+    generate_racecar_module('result.txt', max_time=100, laps=3, track_definition=track_def, car_definition=car_def)
     formula_str = "R{\"total_time\"}min=? [F \"goal\"]"
     print("Generated Prism Program")
     print(datetime.now())
-    print("parsing Prism program with Storm...")
-    program = stormpy.parse_prism_program('result2.txt')
-    print("parsed program")
-    print(datetime.now())
-    print("parsing formulas...")
-    formulas = stormpy.parse_properties_for_prism_program(formula_str, program)
-    print("parsed formulas")
-    print(datetime.now())
-    print("building model...")
-    model = stormpy.build_model(program, formulas)
-    print(model)
-    initial_state = model.initial_states[0]
-    print("built model")
-    print(datetime.now())
-    print("Checking model...")
-    result = stormpy.model_checking(model, formulas[0], extract_scheduler=False)
-    print("Finished checking")
-    print(datetime.now())
-    print(result.at(initial_state))
+    if RUN_STORMPY:
+        import stormpy
+        print("parsing Prism program with Storm...")
+        program = stormpy.parse_prism_program('result2.txt')
+        print("parsed program")
+        print(datetime.now())
+        print("parsing formulas...")
+        formulas = stormpy.parse_properties_for_prism_program(formula_str, program)
+        print("parsed formulas")
+        print(datetime.now())
+        print("building model...")
+        model = stormpy.build_model(program, formulas)
+        print(model)
+        initial_state = model.initial_states[0]
+        print("built model")
+        print(datetime.now())
+        print("Checking model...")
+        result = stormpy.model_checking(model, formulas[0], extract_scheduler=True)
+        print("Finished checking")
+        print(datetime.now())
+        print(result.at(initial_state))
